a76f3bda31ed94e7955e97c679b062e5e96d210c
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2020-04-18 15:33:55 +0200

    extruder calibration

--- Configuration.h
+++ Configuration.h
@@ -609,7 +609,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 3200/0.8, 753.42 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 3200/0.8, 704 }
 // #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 739.7 }
 
 
