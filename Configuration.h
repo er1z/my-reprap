4922a58baa92191044387d821f7ceb64166e2140
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2020-05-14 19:19:56 +0200

    last working

--- Configuration.h
+++ Configuration.h
@@ -852,7 +852,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 
 // @section extruder
@@ -882,8 +882,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200 //180
-#define Y_BED_SIZE 200 //150
+#define X_BED_SIZE 150 //180
+#define Y_BED_SIZE 150 //150
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
