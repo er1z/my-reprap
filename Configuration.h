a8a95ff0490720cd1b3349c9407afed1243b7feb
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2020-07-10 15:35:35 +0200

    updated bed dimensions

--- Configuration.h
+++ Configuration.h
@@ -1094,15 +1094,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 150 //180
-#define Y_BED_SIZE 150 //150
+#define X_BED_SIZE 190 //180
+#define Y_BED_SIZE 185 //150
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS 5
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE-5
 #define Z_MAX_POS 200
 
 /**
